c6ff549c7d75f30d8689bbb651ccf18b09cd7cea
MartinsCristian <cristian.martins@estudante.ufscar.br>
2021-10-30 15:26:30 -0300

    Prototipo_HUD1.0
    na pasta cristoscript tem material e hud material, iniciar a fazer o cursor

---
+++ Projeto Cosmos/Assets/Scripts/CristoScript/CursorController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class CursorController : MonoBehaviour
+{
+    //declaracao das texturas do cursor caso tenha mais de uma etc
+    public Texture2D cursor;
+
+    private void Awake()
+    {
+        ChangeCursor(cursor);
+        cursor.lockState = CursorLockMode.Confined;
+    }
+    private void ChangeCursor(Texture2D cursorType)
+    {
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
+*/
