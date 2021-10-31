8dd1ff0ae4d356c1ca1f687cf20495e85527a115
MartinsCristian <cristian.martins@estudante.ufscar.br>
2021-10-30 23:23:08 -0300

    initialShip1.1

---
+++ Projeto Cosmos/Assets/Cristo/Scripts/CalvoMov.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CalvoMov : MonoBehaviour
+{
+    bool boost;
+    string boost_string;
+    [SerializeField] TMPro.TextMeshProUGUI boostText;
+    int boost_value;
+    public CharacterController controller;
+    public Transform playerTransform;
+    [SerializeField] public float speed = 12f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    float zRotation = 0f;
+
+    private float rollInput;
+    public float lookSpeed = 60f;
+    public float rollSpeed = 130f;
+
+    public Vector2 lookInput, screenCenter, mouseDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boost_value = 1000;
+        boost_string = (boost_value/10).ToString();
+        boostText.text = boost_string;
+        screenCenter.x = Screen.width * .5f;
+        screenCenter.y = Screen.height * .5f;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        boost_string = (boost_value/10).ToString();
+        boostText.text = boost_string;
+        Debug.Log(speed);
+        if(boost_value < 0) {
+            boost_value = 0;
+        }
+        if(Input.GetKey(KeyCode.LeftShift) && boost_value > 20) {
+            speed = 50f;
+            boost_value -= 10;
+        } else {
+            if(boost_value < 1000) {
+                boost_value++;
+            }
+            speed = 12f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Diagonal"), Time.deltaTime);
+
+        transform.Rotate(-mouseDistance.y * lookSpeed * Time.deltaTime, mouseDistance.x * lookSpeed *Time.deltaTime, 
+                        rollInput * rollSpeed * Time.deltaTime, Space.Self);
+
+        // Movimento
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move* speed * Time.deltaTime);
+
+    }
+}
+
---
+++ Projeto Cosmos/Assets/Cristo/Scripts/PortixMov.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortixMov : MonoBehaviour
+{
+    // private float yawTorque = 500f;
+    [Header("--Controles da Nave--")]
+    [SerializeField] private bool inverterControle = true;
+    [Header("---------------------")]
+
+    [SerializeField]private float pitchTorque = 1000f;
+    [SerializeField] private float rollTorque = 1000f;
+    [SerializeField] private float thrust = 100f;
+    private float strafeThrust = 50f;
+    [SerializeField]private float thrustGlideReduction = 100f;
+    private float rollReduction = 0.111f;
+    float glide = 0f;
+    private Vector3 currentVelocity;
+    [SerializeField] public float velocidade = 5f;
+    [SerializeField] private TMPro.TextMeshProUGUI ammoText;
+    [SerializeField] public ProjectileLauncher projectileScript;
+    private string ammoString;
+
+
+    Rigidbody rb;
+    [SerializeField]private float inputRoll = 0f;
+    [SerializeField]private float inputPitch = 0f;
+    private float thrustInput = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        if (!inverterControle)
+            pitchTorque = -pitchTorque;
+    }
+
+    private void Update()
+    {
+        inputRoll = Input.GetAxis("Mouse X");
+        inputPitch = Input.GetAxis("Mouse Y");
+        thrustInput = Input.GetAxis("Vertical");
+        
+        ammoText.text= projectileScript.bulletCount.ToString() + " / " + projectileScript.extraAmmo.ToString();
+
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        // Colocar Drag = 1 e Angular Drag = 2.5 no RigidBody
+         
+        // Roll
+        rb.AddRelativeTorque(Vector3.back * rollTorque * Mathf.Clamp(inputRoll, -1f, 1f) * Time.deltaTime);
+
+
+        // Pitch
+        rb.AddRelativeTorque(Vector3.right * pitchTorque * Mathf.Clamp(inputPitch, -1f, 1f) * Time.deltaTime);
+
+
+        currentVelocity = new Vector3(Input.GetAxis("Horizontal") * velocidade * Time.deltaTime, 0f, Input.GetAxis("Vertical") * velocidade * Time.deltaTime);
+        transform.Translate(currentVelocity);
+
+        /*
+        // Thrust
+        if(thrustInput > 0.1f || thrustInput < -0.1f)
+        {
+            float currentThrust = thrust;
+
+            rb.AddRelativeForce(currentThrust * thrustInput * Time.deltaTime * Vector3.forward);
+            glide = thrust;
+        }
+        else
+        {
+            rb.AddRelativeForce(Vector3.forward * glide * Time.deltaTime);
+            if(glide > 0.1f)
+                glide -= thrustGlideReduction;
+        }
+        //*/
+    }
+}
