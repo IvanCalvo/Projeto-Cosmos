3d961cdd42b843d2fb44d28fcf451167486e43f2
IvanCalvo <ivandc1998.ic@gmail.com>
2021-11-30 14:27:43 -0300

    Update IA
    -Começo da criação da inteligencia artificial do inimigo, ainda tá bem bugada e não tá funcionando corretamente, mas é só um começo

--- Projeto Cosmos/Assets/Scripts/enemyScript.cs
+++ Projeto Cosmos/Assets/Scripts/enemyScript.cs
@@ -5,17 +5,19 @@ using UnityEngine.AI;
 
 public class enemyScript : MonoBehaviour
 {
-    public NavMeshAgent agent;
+    //public NavMeshAgent agent;
+
+    Rigidbody enemyRB;
 
     public Transform player;
 
-    public LayerMask whatIsGround, whatIsPlayer;
+    public LayerMask Objects, whatIsPlayer;
 
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
 
-    public float timeBetweenAttacks = 1f;
+    public float timeBetweenAttacks = .3f;
     bool alreadyAttacked;
 
     public float sightRange, attackRange;
@@ -24,7 +26,8 @@ public class enemyScript : MonoBehaviour
     private void Awake()
     {
         player = GameObject.Find("InicialShip").transform;
-        agent = GetComponent<NavMeshAgent>();
+        enemyRB = GetComponent<Rigidbody>();
+        //agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
@@ -39,9 +42,12 @@ public class enemyScript : MonoBehaviour
 
     private void Patroling()
     {
+        Debug.Log("Idle");
         if(!walkPointSet) SearchWalkPoint();
 
-        if(walkPointSet) agent.SetDestination(walkPoint);
+        //if(walkPointSet) agent.SetDestination(walkPoint);
+
+        enemyRB.AddForce(walkPoint, ForceMode.Force);
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
@@ -55,23 +61,43 @@ public class enemyScript : MonoBehaviour
     {
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
-        //float randomY = Random.Range(-walkPointRange, walkPointRange);
+        float randomY = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y + randomY, transform.position.z + randomZ);
 
-        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) 
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, Objects)) 
         {
+            walkPointSet = false;
+        
+        } else {
+
             walkPointSet = true;
         }
     }
     private void ChasePlayer()
     {
-        agent.SetDestination(player.position);
+        Debug.Log("Chasing");
+        //agent.SetDestination(player.position);
+        enemyRB.AddForce(player.position, ForceMode.Force);
+
+        Vector3 desvio = new Vector3(player.position.x + 1f, player.position.y + 1f, player.position.z + 1f);
+
+        if(Physics.Raycast(player.position, -transform.up, 5f, Objects)) {
+            enemyRB.AddForce(desvio, ForceMode.Force);
+        }
     }
 
     private void AttackPlayer()
     {
-        agent.SetDestination(player.position);
+        //agent.SetDestination(player.position);
+
+        enemyRB.AddForce(player.position, ForceMode.Force);
+
+        Vector3 desvio = new Vector3(player.position.x + 1f, player.position.y + 1f, player.position.z + 1f);
+
+        if(Physics.Raycast(player.position, -transform.up, 5f, Objects)) {
+            enemyRB.AddForce(desvio, ForceMode.Force);
+        }
 
         transform.LookAt(player);
 
