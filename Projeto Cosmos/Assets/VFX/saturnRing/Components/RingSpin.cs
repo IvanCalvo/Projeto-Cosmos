074466f36f3a7289f68a61b366c27e0c13bb8870
DanielPaglia <daniel.pagliaricci@estudante.ufscar.br>
2022-01-05 15:06:00 -0300

    URP_VFX

---
+++ Projeto Cosmos/Assets/VFX/saturnRing/Components/RingSpin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingSpin : MonoBehaviour
+{
+    [SerializeField]
+    float rotationSpeed = 10f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime);
+    }
+}
