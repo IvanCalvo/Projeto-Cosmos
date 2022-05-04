d29fdd6525548132795bae2569b6fd331e915879
IvanCalvo <ivandc1998.ic@gmail.com>
2022-05-04 18:00:50 -0300

    InstructionsScreen
    - Tela de como jogar adicionada (ainda sem nenhum texto)

--- Projeto Cosmos/Assets/Scripts/MenuScript.cs
+++ Projeto Cosmos/Assets/Scripts/MenuScript.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class MenuScript : MonoBehaviour
 {
     public GameObject MenuCanvas;
+    public GameObject InstructionCanvas;
 
     public void Start()
     {
@@ -14,10 +15,23 @@ public class MenuScript : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
        
         MenuCanvas.SetActive(true);
+        InstructionCanvas.SetActive(false);
     }
     public void StartGame()
     {
         SceneManager.LoadScene("DestroyEnemy Scene");
+    }
+
+    public void Instructions()
+    {
+        MenuCanvas.SetActive(false);
+        InstructionCanvas.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        MenuCanvas.SetActive(true);
+        InstructionCanvas.SetActive(false);
     }
 
     public void QuitGame()
