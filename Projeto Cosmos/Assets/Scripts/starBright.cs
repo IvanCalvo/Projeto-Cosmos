42cfaf7129dfba4554a6a81bfedcf65aff2b81a9
IvanCalvo <ivandc1998.ic@gmail.com>
2022-08-15 22:05:10 -0300

    Update starBright.cs

--- Projeto Cosmos/Assets/Scripts/starBright.cs
+++ Projeto Cosmos/Assets/Scripts/starBright.cs
@@ -6,10 +6,40 @@ public class starBright : MonoBehaviour
 {
     public GameObject star;
     bool started;
+    float red;
+    float green;
+    float blue;
 
     void Start()
     {
+        if(Random.Range(0f, 1f) < .2f)
+        {
+            //estrela azulada
+            if(Random.Range(0f, 1f) < .5f)
+            {
+                red = Random.Range(.3f, .5f);
+                green = Random.Range(0f, .3f);
+                blue = Random.Range(.5f, .7f);
+            } 
+            else
+            //estrela avermelhada
+            {
+                red = Random.Range(.5f, .8f);
+                green = Random.Range(0f, .1f);
+                blue = Random.Range(.2f, .5f);
+            }
+            
+        }
+        else
+        {
+            red = 1f;
+            green = 1f;
+            blue = 1f;
+        }
+        SpriteRenderer aux = star.GetComponent<SpriteRenderer>();
+        aux.color = new Color(red, green, blue, Random.Range(.1f, 1f));
         started = false;
+
     }
 
     void Update()
@@ -27,7 +57,7 @@ public class starBright : MonoBehaviour
         float bright = Random.Range(.1f, .6f);
         yield return new WaitForSeconds(time);
         SpriteRenderer sprite = star.GetComponent<SpriteRenderer>();
-        sprite.color = new Color(1f, 1f, 1f, bright);
+        sprite.color = new Color(red, green, blue, bright);
         started = false;
     }
 }
