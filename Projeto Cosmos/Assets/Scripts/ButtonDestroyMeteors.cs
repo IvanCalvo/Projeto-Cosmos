79af1386254cf72cc49a04ba67a013a784a6a6ab
IvanCalvo <ivandc1998.ic@gmail.com>
2022-09-08 12:06:18 -0300

    Update Botões e Missão
    - Missões no menu da estação estão em botões separados, mas um dos botões ainda não está funcionando 100%

--- Projeto Cosmos/Assets/Scripts/ButtonDestroyMeteors.cs
+++ Projeto Cosmos/Assets/Scripts/ButtonDestroyMeteors.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StationHUD : MonoBehaviour
+public class ButtonDestroyMeteors : MonoBehaviour
 {
     Button missionButton;
     int state;
